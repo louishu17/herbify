26fa5613b1e1db317ccad64f3f80649dfc7e6e07
frankwillard <frankiecwillard@gmail.com>
2023-10-04 15:42:41 -0400

    Captions, avatars in posts

--- frontend/herbify/components/pageSpecific/feed/recipeOnFeed.tsx
+++ frontend/herbify/components/pageSpecific/feed/recipeOnFeed.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { RecipeInfoFromFeed } from "@/pages/api/feed";
-import { Typography, Box, Link as MuiLink } from "@mui/material";
+import { Avatar, Typography, Box, Stack, Link as MuiLink } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import { useImageForRecipe } from "@/lib/imageHooks";
@@ -11,6 +11,7 @@ interface RecipeOnFeedProps {
 
 export const RecipeOnFeed : React.FC<RecipeOnFeedProps> = (props : RecipeOnFeedProps) => {
     const {data : imageSrc, isLoading : isLoadingImg, isError : isErrorLoadingImg} = useImageForRecipe(props.info.id);
+    
     return (
         <Link href={"/recipes/"+props.info.id} passHref>
             <MuiLink underline="none">
@@ -26,9 +27,16 @@ export const RecipeOnFeed : React.FC<RecipeOnFeedProps> = (props : RecipeOnFeedP
                     width={275}
                     marginLeft={50}
                 >
-
-                    <Typography variant="h5">{props.info.title}</Typography>
+                    <Stack direction="row" spacing={2} marginBottom={2}>
+                        <Avatar
+                                    src={imageSrc}
+                                    alt="Profile Picture"
+                                    sx={{ width: 50, height: 50, marginBottom: 1 }}
+                                />
+                        <Typography variant="h5">{props.info.title}</Typography>
+                    </Stack>
                     <ImageToDisplay imageSrc={imageSrc ? imageSrc : ""} isLoading={isLoadingImg} isError={isErrorLoadingImg}/>
+                    <Typography variant="h5" marginTop={3} color={"red"}>{props.info.caption}</Typography>
                 </Box>
             </MuiLink>
         </Link>
--- frontend/herbify/pages/api/feed.ts
+++ frontend/herbify/pages/api/feed.ts
@@ -7,16 +7,17 @@ export interface FeedData{
 
 export interface RecipeInfoFromFeed {
     id : number;
-    title : string;
+    title: string;
+    caption: string;
 }
 
 export default function getFeedRecipeIDs(req: NextApiRequest, res: NextApiResponse<FeedData>) {
     setTimeout(() => {
         res.status(200).json({descriptions : [
-            {id : 1, title: "Pasta"},
-            {id : 2, title : "Linguini"}, 
-            {id : 3, title : "Herb special"}, 
-            {id : 4, title : "Louis special"}
+            {id : 1, title: "Pasta", caption: "Yum!"},
+            {id : 2, title: "Linguini", caption: "Delicious!"}, 
+            {id : 3, title: "Herb special", caption: "Yum!"}, 
+            {id : 4, title: "Louis special", caption: "Louis better than Herb"}
         ]})
 
     }, 5200);
