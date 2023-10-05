c307a8d3a87f26fc4acd90cf7c342cc312681374
louishu17 <lh329@duke.edu>
2023-10-05 17:07:02 -0400

    recipe ui

--- frontend/herbify/pages/recipes/create.tsx
+++ frontend/herbify/pages/recipes/create.tsx
@@ -5,28 +5,47 @@ import {DirectionsForm} from "@/components/pageSpecific/create/addDirectionsForm
 import { createContext, useState } from "react";
 import { NewRecipeContext } from "@/lib/createRecipePage/newRecipeContext";
 import { AddTitleForm } from "@/components/pageSpecific/create/addTitleForm";
-
+import axios from 'axios';
 
 export default function CreateRecipePage() {
+    const [errorMessage, setErrorMessage] = useState<string>("");
     const [ingredients, setIngredients] = useState<string[]>(['']);
     const [directions, setDirections] = useState<string[]>(['']);
     const [title, setTitle] = useState<string>('');
+    const [caption, setCaption] = useState<string>('');
+
+    const createRecipe = async () => {
+        try {
+            const recipe_data = {
+                postedByUserID: 1,
+                title: title,
+                caption: caption,
+                ingredients: ingredients,
+                steps: directions
+            }
+
+            const response = await axios.post('http://127.0.0.1:5000/create-recipe', recipe_data);
+        }
+        catch (error) {
+            console.error(error);
+            setErrorMessage("An unexpected error occurred");
+        }
+    }
+
     const handleSubmit = () => {
-        console.log(ingredients);
-        console.log(directions);
+        createRecipe();
     }
 
     return (
         <BaseHerbifyLayoutWithTitle title="Create Recipe">
-            <NewRecipeContext.Provider value={{title, setTitle, ingredients, setIngredients, directions, setDirections}}>
+            <NewRecipeContext.Provider value={{title, setTitle, caption, setCaption, ingredients, setIngredients, directions, setDirections}}>
                 <Container maxWidth="lg" >
                     <AddTitleForm/>
                     <Stack direction={"row"} spacing={6} >
                         <IngredientsForm/>
                         <DirectionsForm/>
-                        
                     </Stack>
-                    <Button variant="contained" onClick={handleSubmit} style={{backgroundColor: "Highlight", marginTop : 40}}>Submit Recipe</Button>
+                    <Button variant="contained" onClick={handleSubmit} style={{backgroundColor: "Highlight", marginTop : 40, display: "block", margin: "0 auto"}}>Submit Recipe</Button>
                 </Container>
             </NewRecipeContext.Provider>
         </BaseHerbifyLayoutWithTitle>
