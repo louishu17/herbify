b32bcbaa500b9194c12d6d138b5d17ad34195d50
Keith Cressman <keith.cressman@duke.edu>
2023-11-27 14:17:16 -0500

    fixed typo in leaderboard hook

--- frontend/herbify/lib/leaderboardHooks.ts
+++ frontend/herbify/lib/leaderboardHooks.ts
@@ -8,12 +8,12 @@ import axios from 'axios';
 const fetchMockLeaderboard = async () : Promise<LeaderboardAPIData> => {
     const response = await fetch('/api/leaderboard');
     if (response.status > 300){
-        throw new Error("Error fetching feed");
+        throw new Error("Error fetching leaderboard");
     } 
     return response.json();
 }
 
 
 export const useFetchMockLeaderboard = () : UseQueryResult<LeaderboardAPIData> => {
-    return useQuery<LeaderboardAPIData>("fetchFeed", fetchMockLeaderboard);
+    return useQuery<LeaderboardAPIData>("fetchLeaderboard", fetchMockLeaderboard);
 }
