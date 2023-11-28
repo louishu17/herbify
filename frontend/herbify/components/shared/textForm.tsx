0fdc3a4e02ee81b31f72a2f9c1c7a02e170181e0
frankwillard <frankiecwillard@gmail.com>
2023-11-27 21:08:45 -0500

    Fix birthday formatting for settings

--- frontend/herbify/components/shared/textForm.tsx
+++ frontend/herbify/components/shared/textForm.tsx
@@ -1,70 +1,57 @@
-// LoginForm.tsx
-
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import {  TextField, Typography, Button, Container } from '@mui/material';
-
-
+import { TextField, Typography, Button, Container } from '@mui/material';
 
 interface HerbifyFormProps<FormValuesInterface> {
-    handleSubmit : (values : FormValuesInterface ) => void;
-    initialValues : FormValuesInterface;
-    validationSchema : any;
-    textFields? : TextFieldProps[];
-    errorMessage : string;
+    handleSubmit: (values: FormValuesInterface) => void;
+    initialValues: FormValuesInterface;
+    validationSchema: any;
+    textFields?: TextFieldProps[];
+    errorMessage: string;
 }
 
 interface TextFieldProps {
-    name : string;
-    type : string;
+    name: string;
+    type: string;
 }
 
 export const NO_ERROR_MESSAGE = "";
 
-export const HerbifyForm: React.FC<HerbifyFormProps<any>> = (props:HerbifyFormProps<any>) => {
-
+export const HerbifyForm: React.FC<HerbifyFormProps<any>> = ({ handleSubmit, initialValues, validationSchema, textFields, errorMessage }) => {
     return (
         <Container maxWidth="sm">
             <Formik
-                initialValues={props.initialValues}
-                validationSchema={props.validationSchema}
-                onSubmit={(values : any) => {
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={(values) => {
                     console.log("form was submitted");
-                    props.handleSubmit(values);
+                    handleSubmit(values);
                 }}
                 enableReinitialize={true}
             >
                 {() => (
                     <Form>
-                        {props.textFields?.map((fieldProps, index) => {
-                            return (
-                                <div key={index} style={{marginBottom : 4}}>
-                                    <Field
-                                        as={TextField}
-                                        type={fieldProps.type}
-                                        label={fieldProps.name}
-                                        name={fieldProps.name}
-                                        variant="outlined"
-                                        fullWidth
-                                        required
-                                    />
-                                    <ErrorMessage name={fieldProps.name} component="Typography" />
-                                </div>
-                            );
-                        })}
-
-                    
-                        {props.errorMessage.length > 0 ? <Typography color="red">{props.errorMessage}</Typography> : null}
-
-                        <Button type="submit"  variant='contained' style={{backgroundColor : "Highlight"}} >
+                        {textFields?.map((fieldProps, index) => (
+                            <div key={index} style={{ marginBottom: 4 }}>
+                                <Field
+                                    as={TextField}
+                                    type={fieldProps.type}
+                                    label={fieldProps.name}
+                                    name={fieldProps.name}
+                                    variant="outlined"
+                                    fullWidth
+                                    required
+                                />
+                                <ErrorMessage name={fieldProps.name} component="Typography" />
+                            </div>
+                        ))}
+                        {errorMessage && <Typography color="red">{errorMessage}</Typography>}
+                        <Button type="submit" variant='contained' style={{ backgroundColor: "Highlight" }}>
                             Submit
                         </Button>
-                        
                     </Form>
                 )}
             </Formik>
         </Container>
-    
     );
 };
-
