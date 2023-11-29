ffeff18ed214ec84dbc76c083fc6d762d9322f0c
louishu17 <lh329@duke.edu>
2023-11-29 14:38:43 -0500

    Infinite Scroll

--- frontend/herbify/pages/feed.tsx
+++ frontend/herbify/pages/feed.tsx
@@ -3,12 +3,29 @@ import { BaseHerbifyLayoutWithTitle } from "@/components/shared/layouts/baseLayo
 import { HerbifyLoadingCircle, HerbifyLoadingContainer } from "@/components/shared/loading";
 import { useFetchPaginatedFeed} from "@/lib/feedHooks";
 import {Typography, Container, Button} from '@mui/material';
-import React from "react";
+import React, {useEffect, useRef} from "react";
 
 
 export default function FeedPage() {
     //const {data : recipes, isLoading, isError} = useFetchBasicFeed();
     const {data : recipes, isLoading, isError, loadMore} = useFetchPaginatedFeed();
+    const loader = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            if (entries[0].isIntersecting && !isLoading && recipes) {
+                loadMore();
+            }
+        }, {threshold: 1});
+        if (loader.current) {
+            observer.observe(loader.current)
+        }
+        return () => {
+            if (loader.current) {
+                observer.unobserve(loader.current)
+            }
+        }
+    }, [loadMore, isLoading])
 
     let body = null;
     if (!recipes){
@@ -17,8 +34,9 @@ export default function FeedPage() {
         body = (
             <Container style={{alignContent: 'center'}} maxWidth="lg">
                 {recipes.descriptions.map((recipe) => <RecipeOnFeed info={recipe} key={recipe.id}/>)}
-                {isLoading ? <HerbifyLoadingCircle/> : null}
-                <Button onClick={() => loadMore()} variant="contained" style={{paddingTop : 16, paddingBottom : 16, backgroundColor: "green"}}>Load More</Button>
+                <div ref={loader} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    {isLoading ? <HerbifyLoadingCircle/> : null}
+                </div>
             </Container>
         )
     } else {
