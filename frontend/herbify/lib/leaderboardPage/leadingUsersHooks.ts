37a9f87f2047068c958ffa48733657b1a6ca1588
Keith Cressman <keith.cressman@duke.edu>
2023-12-09 18:57:38 -0500

    leaderboard for posts

---
+++ frontend/herbify/lib/leaderboardPage/leadingUsersHooks.ts
@@ -0,0 +1,31 @@
+import {useQuery, UseQueryResult} from "react-query";
+import { LeaderboardAPIData } from "@/pages/api/leaderboard";
+import axios from 'axios';
+import { FeedData } from "@/pages/api/feed";
+
+
+const fetchLocallyRunningLeadingUsers = async () : Promise<LeaderboardAPIData> => {
+    const response = await axios.get('http://127.0.0.1:5000/leading_users')
+
+    if (response.status > 300){
+        throw new Error("Error fetching leading users");
+    } 
+    return response.data;
+}
+
+const fetchMockLeadingUsers = async () : Promise<LeaderboardAPIData> => {
+    const response = await fetch('/api/leaderboard');
+    if (response.status > 300){
+        throw new Error("Error fetching leaderboard");
+    } 
+    return response.json();
+}
+
+
+export const useFetchMockLeadingUsers = () : UseQueryResult<LeaderboardAPIData> => {
+    return useQuery<LeaderboardAPIData>("fetchLeadingUsers", fetchMockLeadingUsers);
+}
+
+export const useFetchLocallyRunningLeadingUsers = () : UseQueryResult<LeaderboardAPIData> => {
+    return useQuery<LeaderboardAPIData>("fetchLeadingUsers", fetchLocallyRunningLeadingUsers);
+}
