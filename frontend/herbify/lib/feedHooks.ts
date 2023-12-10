7841f60e4d0f0993f80713d0383bf6eac1ceaa33
Keith Cressman <keith.cressman@duke.edu>
2023-12-09 22:07:55 -0500

    ratings on feed

--- frontend/herbify/lib/feedHooks.ts
+++ frontend/herbify/lib/feedHooks.ts
@@ -55,11 +55,17 @@ export const useFetchPaginatedFeed = () : usePaginatedFeedResult => {
     const [lastPageNumber, setLastPageNumber] = useState<number>(0);
     const [fetchingCustomized, setFetchingCustomized] = useState<boolean>(true);
     const [pageNumber, setPageNumber] = useState<number>(0);
+    const [descriptions, setDescriptions] = useState<RecipeInfoFromFeed[]>([]);
 
     const {data : queryData, isLoading, isFetchingNextPage , isError, refetch, fetchNextPage} = useInfiniteQuery<FeedData>(
         ["fetchFeed", pageNumber, fetchingCustomized], 
         ({pageParam = 0}) => fetchLocallyRunningPaginatedFeed(pageParam, fetchingCustomized), 
         {
+            onSuccess : (data) => {
+                let newRecipes : RecipeInfoFromFeed[] = [];
+                data.pages.forEach((recipes) => recipes.descriptions.forEach((recipe) => descriptions.push(recipe)));
+                setDescriptions([...descriptions, ...newRecipes])
+            },
             getNextPageParam: (lastPage, pages) => {
                 if (fetchingCustomized && lastPage.descriptions.length < 7){
                     setFetchingCustomized(false);
@@ -70,11 +76,24 @@ export const useFetchPaginatedFeed = () : usePaginatedFeedResult => {
             }
         },
     );
-    const descriptions : RecipeInfoFromFeed[] = [];
-    if (queryData) {
-        queryData.pages.forEach((recipes) => recipes.descriptions.forEach((recipe) => descriptions.push(recipe)));
-    }
+
     return {data : {descriptions : descriptions}, isLoading : isLoading || isFetchingNextPage, isError, loadMore : fetchNextPage}
+}
+
+interface UserLikedRecipeAPIData {
+    userLiked : boolean;
+}
+const fetchIfUserLikedRecipe = async (recipeID : number) : Promise<UserLikedRecipeAPIData> => {
+    const response = await axios.get('http://127.0.0.1:5000/check_user_liked/' + recipeID, {withCredentials : true})
+    if (response.status > 300){
+        throw new Error("Error fetching feed");
+    } 
+    return response.data;
+}
+
+export const useCheckIfUserLikedRecipe = (recipeID : number) : UseQueryResult<UserLikedRecipeAPIData> => {
+    return useQuery(["fetch if user liked recipe", recipeID], () => fetchIfUserLikedRecipe(recipeID))
+
 }
 
 
