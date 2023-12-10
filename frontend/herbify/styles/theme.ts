c230ff80c0459bfa738adc93c324d7bf10c2c09e
Herbert Wang <hjw18@duke.edu>
2023-12-09 19:20:10 -0500

    create-recipe bugfixes and ui

--- frontend/herbify/styles/theme.ts
+++ frontend/herbify/styles/theme.ts
@@ -1,25 +1,39 @@
 import { createTheme } from '@mui/material/styles';
 
-
 const theme = createTheme({
-    components: {
-      MuiButton: {
-        styleOverrides: {
-          contained: {
-            '&': {
-              borderRadius: '20px',
-              color: 'white',
-              backgroundColor: '#05353B!important',
-              '&:hover': {
-                backgroundColor: '#05353B',
-              },
-            },
+  typography: {
+    h3: {
+      color: '#05353B',
+    },
+    h4: {
+      color: '#05353B',
+    },
+    h5: {
+      color: '#05353B',
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        contained: {
+          borderRadius: '20px',
+          color: 'white',
+          backgroundColor: '#05353B!important',
+          '&:hover': {
+            backgroundColor: '#05353B',
+          },
+        },
+        outlined: {
+          borderRadius: '20px',
+          color: '#05353B',
+          borderColor: '#05353B',
+          '&:hover': {
+            borderColor: '#05353B',
           },
         },
       },
     },
-  });
-  
+  },
+});
 
 export default theme;
-
