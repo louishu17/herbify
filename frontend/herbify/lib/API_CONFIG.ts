78fe06d248e3179110bde1200d7ff2c1cba155d4
Keith Cressman <keith.cressman@duke.edu>
2023-12-21 21:29:28 -0500

    changed env variable name

--- frontend/herbify/lib/API_CONFIG.ts
+++ frontend/herbify/lib/API_CONFIG.ts
@@ -4,8 +4,8 @@ export const API_ROUTE = "http://localhost:5000/";
 
 // Initialize the AWS SDK
 AWS.config.update({
-    accessKeyId: process.env.AWS_S3_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_KEY,
     region: 'us-east-1'
 });
   
